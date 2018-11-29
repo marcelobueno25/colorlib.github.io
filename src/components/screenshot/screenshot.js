22836f2cfd06c716c6c60322aefec1c3d9f0a008
Marcelo Bueno <marcelobueno25@hotmail.com>
2018-11-29 16:47:05 -0200

    Metade

---
+++ src/components/screenshot/screenshot.js
@@ -0,0 +1,44 @@
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 5,
+//     spaceBetween: 50,
+//     loop: true,
+//     // init: false,
+//     autoplay: {
+//       delay: 4500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       1024: {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//       },
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//       },
+//       320: {
+//         slidesPerView: 1,
+//         spaceBetween: 10,
+//       }
+//     }
+//   });
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
