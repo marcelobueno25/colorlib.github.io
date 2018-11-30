a1af144f72f81e4c157a19e8acff401770b458f3
Marcelo Bueno <marcelobueno25@hotmail.com>
2018-11-30 16:40:17 -0200

    ajuste

--- src/components/screenshot/screenshot.js
+++ src/components/screenshot/screenshot.js
@@ -32,11 +32,15 @@
 //   });
 
 
-  var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.screenshot .swiper-container', {
     slidesPerView: 5,
     centeredSlides: true,
     spaceBetween: 30,
     loop: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
